ef8676cada0fafd1fd5e882737241b2b9a0a4bbe
Walker Case <walker.case3323@gmail.com>
2024-01-17 17:49:25 -0600

    Added 3d jewelery and gem models!

--- src/main/java/com/walkercase/jeweler/item/jewelry/JewelerAmulet.java
+++ src/main/java/com/walkercase/jeweler/item/jewelry/JewelerAmulet.java
@@ -1,10 +1,12 @@
 package com.walkercase.jeweler.item.jewelry;
 
+import com.walkercase.jeweler.JewelerMain;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 
 public class JewelerAmulet extends JewelerItemBase {
-    public JewelerAmulet(Rarity rarity, Item repairItem, int durability, TexturePosition texturePosition) {
-        super(rarity, repairItem, durability, texturePosition);
+    public JewelerAmulet(Rarity rarity, Item repairItem, int durability) {
+        super(rarity, repairItem, durability, new ResourceLocation(JewelerMain.MODID, "item/amulet"));
     }
 }
--- src/main/java/com/walkercase/jeweler/item/jewelry/JewelerRing.java
+++ src/main/java/com/walkercase/jeweler/item/jewelry/JewelerRing.java
@@ -1,10 +1,12 @@
 package com.walkercase.jeweler.item.jewelry;
 
+import com.walkercase.jeweler.JewelerMain;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 
 public class JewelerRing extends JewelerItemBase {
-    public JewelerRing(Rarity rarity, Item repairItem, int durability, TexturePosition texturePosition) {
-        super(rarity, repairItem, durability, texturePosition);
+    public JewelerRing(Rarity rarity, Item repairItem, int durability) {
+        super(rarity, repairItem, durability, new ResourceLocation(JewelerMain.MODID, "item/ring"));
     }
 }
