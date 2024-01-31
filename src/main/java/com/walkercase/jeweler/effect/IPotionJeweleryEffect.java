6b75f0c529a555d3745ee123c53899ca06873ec0
Walker Case <walker.case3323@gmail.com>
2024-01-30 19:17:13 -0600

    Updated IPotionJeweleryEffect durations.

--- src/main/java/com/walkercase/jeweler/effect/IPotionJeweleryEffect.java
+++ src/main/java/com/walkercase/jeweler/effect/IPotionJeweleryEffect.java
@@ -5,6 +5,7 @@ import com.walkercase.jeweler.item.jewelry.JewelerItemBase;
 import net.minecraft.core.particles.DustParticleOptions;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
@@ -34,13 +35,14 @@ public interface IPotionJeweleryEffect extends IJewelryEffect {
         int level = EffectAPI.getEffectValue(this, stack);
 
         MobEffectInstance effectInstance = slotContext.entity().getEffect(effect);
-        int duration = 30 * level;
-        if(effectInstance == null || effectInstance.getDuration() < 30){
+        int duration = 600 * level;
+        if(effectInstance == null || effectInstance.getDuration() < 300){
             slotContext.entity().addEffect(new MobEffectInstance(effect, duration, level-1));
 
+            slotContext.entity().playSound(SoundEvents.BUBBLE_COLUMN_BUBBLE_POP);
             this.playParticles(slotContext.entity().level, slotContext.entity(), getEquipParticle(), 20, 0.5d);
 
-            IJewelryEffect.damageStack((Player)slotContext.entity(), stack, RANDOM, level);
+            IJewelryEffect.damageStack((Player)slotContext.entity(), stack, RANDOM, level * 3);
         }
     }
 
