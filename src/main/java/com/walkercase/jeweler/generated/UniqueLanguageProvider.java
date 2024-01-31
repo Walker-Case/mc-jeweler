37fde6cb870db9a9d72a2d1297736364d1ae04bf
Walker Case <walker.case3323@gmail.com>
2024-01-30 19:15:57 -0600

    Moved lang into new package to prevent confict.

--- src/main/java/com/walkercase/jeweler/generated/UniqueLanguageProvider.java
+++ src/main/java/com/walkercase/jeweler/generated/UniqueLanguageProvider.java
@@ -12,35 +12,32 @@ import net.minecraftforge.common.data.LanguageProvider;
 public class UniqueLanguageProvider extends LanguageProvider {
 
     public final JsonObject i18n;
-    public static final boolean run = true;
     public UniqueLanguageProvider(PackOutput output, String modid, String locale) {
-        super(output, modid, locale);
+        super(output, modid + "_lang", locale);
         i18n = AssetAPI.readAsset(new ResourceLocation(modid, "lang/" + locale)).getAsJsonObject();
     }
 
     @Override
     protected void addTranslations() {
-        //RUN ONLY WHEN NEEDED AND COPY ADDITIONS
-        if(run){
-            JsonObject lang = AssetAPI.readAsset(new ResourceLocation(JewelerMain.MODID, "lang/en_us")).getAsJsonObject();
-            EffectAPI.EFFECTS.stream().filter(x->x.getEffectType() == IJewelryEffect.EffectType.NEGATIVE).forEach(e->{
-                this.addTranslation(lang,e.effectID().getNamespace() + ".unique." + e.effectID().getPath(),
-                        i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
-            });
+        JsonObject lang = AssetAPI.readAsset(new ResourceLocation(JewelerMain.MODID, "lang/en_us")).getAsJsonObject();
+        EffectAPI.EFFECTS.stream().filter(x->x.getEffectType() == IJewelryEffect.EffectType.NEGATIVE).forEach(e->{
+            this.addTranslation(lang,e.effectID().getNamespace() + ".unique." + e.effectID().getPath(),
+                    i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
+        });
 
-            EffectAPI.EFFECTS.stream().filter(x->x.getEffectType() == IJewelryEffect.EffectType.POSITIVE).forEach(e->{
-                this.addTranslation(lang,e.effectID().getNamespace() + ".unique." + e.effectID().getPath() + "_0",
-                        "Weak " + i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
-                this.addTranslation(lang, e.effectID().getNamespace() + ".unique." + e.effectID().getPath() + "_1",
-                        i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
-                this.addTranslation(lang, e.effectID().getNamespace() + ".unique." + e.effectID().getPath() + "_2",
-                        "Strong " + i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
+        EffectAPI.EFFECTS.stream().filter(x->x.getEffectType() == IJewelryEffect.EffectType.POSITIVE).forEach(e->{
+            this.addTranslation(lang,e.effectID().getNamespace() + ".unique." + e.effectID().getPath() + "_0",
+                    "Weak " + i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
+            this.addTranslation(lang, e.effectID().getNamespace() + ".unique." + e.effectID().getPath() + "_1",
+                    i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
+            this.addTranslation(lang, e.effectID().getNamespace() + ".unique." + e.effectID().getPath() + "_2",
+                    "Strong " + i18n.get("effect.jeweler." + e.effectID().getPath()).getAsString() + " Focus");
 
-            });
-        }
+        });
     }
 
     private void addTranslation(JsonObject lang, String key, String text){
+        JewelerMain.LOGGER.info("Adding translation " + key + "=" + text);
         if(!lang.has(key))
             this.add(key, text);
     }
